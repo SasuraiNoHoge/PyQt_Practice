fffe70d8bdf386098b19938f95ea8896549bf8f6
SasuraiNoHoge <yanaseson@yanasesonnoiMac.local>
2019-11-30 05:14:57 +0900

    真っ赤

--- cgtest.py
+++ cgtest.py
@@ -15,15 +15,26 @@ def main():
         print('Failed to create window')
         return
 
-    # コンテキストを作成します
+    # コンテキストを作成
     glfw.make_context_current(window)
 
-    # OpenGLのバージョン等を表示します
-    print('Vendor :', glGetString(GL_VENDOR))
-    print('GPU :', glGetString(GL_RENDERER))
-    print('OpenGL version :', glGetString(GL_VERSION))
+    # バージョンを指定
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 0)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
 
-    # ウィンドウを破棄してGLFWを終了します
+    while not glfw.window_should_close(window):
+        # バッファを指定色で初期化
+        glClearColor(1, 0, 0, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # バッファを入れ替えて画面を更新
+        glfw.swap_buffers(window)
+
+        # イベントを受け付けます
+        glfw.poll_events()
+
+    # ウィンドウを破棄してGLFWを終了
     glfw.destroy_window(window)
     glfw.terminate()
 
